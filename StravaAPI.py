4ca7e4bc32dc1d71bb9f277c2d29e4a0714765e2
Cormac Roche <comroche@gmail.com>
2023-04-23 19:45:57 +0100

    Initial check in

---
+++ StravaAPI.py
@@ -0,0 +1,117 @@
+import requests
+import json
+
+
+def makeRequest(method, url, **kwargs):
+    response = requests.request(method, url, **kwargs)
+    if response.status_code == 401:
+        newAccessToken = refreshTokens()
+        kwargs['headers']['Authorization'] = 'Bearer ' + newAccessToken
+        response = requests.request(method, url, **kwargs)
+    return response
+
+
+def readTokens():
+    with open('tokens.json') as file:
+        data = json.load(file)
+    return data
+
+
+def writeTokens(access_token, refresh_token):
+    with open('tokens.json', 'r+') as file:
+        data = json.load(file)
+        data['access_token'] = access_token
+        data['refresh_token'] = refresh_token
+        file.seek(0)
+        json.dump(data, file, indent=4)
+        file.truncate()
+
+
+def refreshTokens():
+    
+    tokens = readTokens()
+    url = tokens['base_url'] + "/oauth/token"
+
+    payload={'client_id': tokens['client_id'],
+    'client_secret': tokens['client_secret'],
+    'grant_type': 'refresh_token',
+    'refresh_token': tokens['refresh_token']}
+
+    response = requests.request("POST", url, data=payload)
+
+    response = json.loads(response.text)
+    writeTokens(response['access_token'], response['refresh_token'])
+    return response['access_token']
+
+
+def getLoggedInAthlete():
+    
+    tokens = readTokens()
+    url = tokens['base_url'] + "/athlete"
+
+    headers = {
+    'Authorization': 'Bearer ' + tokens['access_token'],
+    }
+
+    response = makeRequest("GET", url, headers=headers)
+    return json.loads(response.text)
+
+
+def getLoggedInAthleteActivities():
+    
+    tokens = readTokens()
+    url = tokens['base_url'] + "/athlete/activities"
+
+    headers = {
+    'Authorization': 'Bearer ' + tokens['access_token'],
+    }
+
+    params = {
+        'per_page': 20,
+        'page': 1
+    }
+
+    response = makeRequest("GET", url, headers=headers, params=params)
+    return json.loads(response.text)
+
+
+def getActivityById(activityID):
+    
+    tokens = readTokens()
+    url = tokens['base_url'] + "/activities/" + str(activityID)
+
+    headers = {
+    'Authorization': 'Bearer ' + tokens['access_token'],
+    }
+
+    response = makeRequest("GET", url, headers=headers)
+    return json.loads(response.text)
+
+
+def getActivityStreams(activityID, streamTypes):
+    
+    tokens = readTokens()
+    url = tokens['base_url'] + "/activities/" + str(activityID) + "/streams"
+    streamTypes = str(streamTypes).replace("'","").replace(" ","")[1:-1]
+
+    headers = {
+    'Authorization': 'Bearer ' + tokens['access_token'],
+    }
+    
+    params = {
+        'keys': streamTypes
+    }
+
+    response = makeRequest("GET", url, headers=headers, params=params)
+    return json.loads(response.text)
+
+def updateActivityDescription(activityID, description):
+    
+    tokens = readTokens()
+    url = tokens['base_url'] + "/activities/" + str(activityID)
+
+    payload = {'description': description}
+    headers = {'Authorization': 'Bearer ' + tokens['access_token']}
+    
+    response = makeRequest("PUT", url, headers=headers, data=payload)
+    return json.loads(response.text)
---
+++ main.py
@@ -0,0 +1,96 @@
+import StravaAPI
+import utm
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import geopy.distance
+
+def isHillInRange(hill, start, walkLength):
+    distance = geopy.distance.geodesic((hill[1], hill[2]), (start[0], start[1])).m
+    return distance <= walkLength
+
+def isHillBagged(hill, points, n):
+    for point in points.transpose():
+        d = geopy.distance.geodesic((hill[1], hill[2]), (point[0], point[1])).m
+        if d <= 20 + 3*n:
+            return True
+    return False
+
+
+def checkActivityForHills(activityID, plot=False, n=1):
+    
+    stream = StravaAPI.getActivityStreams(activityID, ['latlng'])
+
+    distIndex = next((index for (index, d) in enumerate(stream) if d["type"] == "distance"), None)
+    latLngIndex = next((index for (index, d) in enumerate(stream) if d["type"] == "latlng"), None)
+
+    streamLatLng = np.array(stream[latLngIndex]['data'])
+    walkLength = stream[distIndex]['data'][-1]
+
+    lat = streamLatLng[:,0][0::n]
+    lng = streamLatLng[:,1][0::n]
+
+    data_set = pd.read_csv('hills.csv')
+    data_frames = pd.DataFrame(data_set)
+    hills_array = np.array(data_frames.values)
+    hills_array = np.array([hill for hill in hills_array if isHillInRange(hill, [lat[0], lng[0]], walkLength)])
+    hills_array = np.array([hill for hill in hills_array if isHillBagged(hill, np.array([lat, lng]), n)])
+
+    if plot:
+        route_cartesian = utm.from_latlon(lat,lng)
+        minX = min(route_cartesian[0])
+        minY = min(route_cartesian[1])
+        x = [i - minX for i in route_cartesian[0]]
+        y = [i - minY for i in route_cartesian[1]]
+        plt.plot(x, y, 'r-')
+
+        if hills_array.size != 0:
+
+            hills_lat = np.array(hills_array[:,1]).astype(float)
+            hills_lng = np.array(hills_array[:,2]).astype(float)
+
+            hills_cartesian = utm.from_latlon(hills_lat,hills_lng)
+            hills_cartesian_x = [i - minX for i in hills_cartesian[0]]
+            hills_cartesian_y = [i - minY for i in hills_cartesian[1]]
+
+            plt.plot(hills_cartesian_x, hills_cartesian_y, 'o')
+
+        ax = plt.gca()
+        ax.set_aspect('equal', adjustable='box')
+        plt.show()
+
+    if hills_array.size != 0:
+        return hills_array[:,0]
+    else:
+        return []
+
+
+def populateDescription(activityID, hills, custom_description = ""):
+
+        hills = ['✅ ' + hill for hill in hills]
+        description = '\n'.join(hills)
+        description = custom_description + 'VLs:\n' + description
+        
+        StravaAPI.updateActivityDescription(activityID, description)
+
+def getCustomDescription(activityID):
+    activity = StravaAPI.getActivityById(activityID)
+    old_description = activity['description']
+    if old_description != "" and 'VLs:' in old_description:
+        old_description = old_description[0:old_description.find('VLs:')]
+    return old_description
+
+def main():
+    activities = StravaAPI.getLoggedInAthleteActivities()
+    hikes = [activity for activity in activities if activity['sport_type']  in ['Hike', 'Walk', 'Run', 'Trail Run']]
+    activityIDs = [hike['id'] for hike in hikes]
+
+    for activityID in activityIDs:
+        hills = checkActivityForHills(activityID, plot=False, n=10)
+        if len(hills) != 0:
+            custom_description = getCustomDescription(activityID)
+            #print(hills, activityID)
+            populateDescription(activityID, hills, custom_description = custom_description)
+
+if __name__ == '__main__':
+    main()
