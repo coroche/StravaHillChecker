233b250fdc4adf080cbec76a2117a04aad5ad2eb
Cormac Roche <comroche@gmail.com>
2023-05-01 21:21:46 +0100

    add listener startup

--- startWebhookListener.sh
+++ startWebhookListener.sh
@@ -1,5 +1,5 @@
 #kill existing listeners / ngrok urls
-for pid in $(ps aux | grep "sudo nohup node strava-webhooks/index.js" | grep -v grep | awk '{print $2}'); do kill -9 $pid; done
+for pid in $(ps aux | grep "strava-webhooks/index.js" | grep -v grep | awk '{print $2}'); do sudo kill -9 $pid; done
 for pid in $(ps aux | grep "ngrok http 1000" | grep -v grep | awk '{print $2}'); do kill -9 $pid; done
 
 #delete old log files
@@ -14,3 +14,14 @@ nohup ngrok http 1000 --log=stdout > logs/ngrok.txt &
 
 #Grab public ngrok URL
 python updateCallbackUrl.py
+
+#Wait until Strava app is updated
+domain=$(jq .webhook_callback_url tokens.json)
+domain="\"""${domain:9}"
+printf "\nUpdate your Strava app with the new authorization domain\n"
+printf "https://www.strava.com/settings/api\n"
+printf "Auth domain: $domain\n"
+read -p "Once updated, press enter to continue"
+
+#Create event subscription
+python setNewEventSubscription.py
--- webhookReceiver.py
+++ webhookReceiver.py
@@ -1,6 +1,6 @@
 try:
-    import activityFunctions
     import sys
+    import activityFunctions
 
     hillFound = activityFunctions.processActivity(sys.argv[1])
     print('Activity:' + sys.argv[1] + ' processed')
