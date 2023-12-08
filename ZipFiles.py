2115a03eb613fc705569d1619eabce32a4428fe9
Cormac Roche <comroche@gmail.com>
2023-12-08 23:07:40 +0000

    Firestore integration

--- ZipFiles.py
+++ ZipFiles.py
@@ -6,19 +6,19 @@ def zip_files(file_paths, zip_name, file_names):
             zipf.write(file, file_names[i])
 
 #Zip files for the stravaWebhook cloud function
-files_to_zip = ['config.json', 'config.py', 'main_stravaWebhook.py', 'requirements.txt']
-archive_file_names = ['config.json', 'config.py', 'main.py', 'requirements.txt']
+files_to_zip = ['config.json', 'config.py', 'main_stravaWebhook.py', 'requirements.txt', 'firebaseServiceAccountKey.json']
+archive_file_names = ['config.json', 'config.py', 'main.py', 'requirements.txt', 'firebaseServiceAccountKey.json']
 zip_file_name = 'stravaWebhook.zip'
 zip_files(files_to_zip, zip_file_name, archive_file_names)
 
 #Zip files for the processActivity cloud function
-files_to_zip = ['activityFunctions.py', 'config.json', 'config.py', 'credentials.json', 'googleSheetsAPI.py', 'main_processActivity.py', 'requirements.txt', 'StravaAPI.py', 'token.json']
-archive_file_names = ['activityFunctions.py', 'config.json', 'config.py', 'credentials.json', 'googleSheetsAPI.py', 'main.py', 'requirements.txt', 'StravaAPI.py', 'token.json']
+files_to_zip = ['activityFunctions.py', 'config.json', 'config.py', 'credentials.json', 'googleSheetsAPI.py', 'main_processActivity.py', 'requirements.txt', 'StravaAPI.py', 'token.json', 'firebaseServiceAccountKey.json']
+archive_file_names = ['activityFunctions.py', 'config.json', 'config.py', 'credentials.json', 'googleSheetsAPI.py', 'main.py', 'requirements.txt', 'StravaAPI.py', 'token.json', 'firebaseServiceAccountKey.json']
 zip_file_name = 'processActivity.zip'
 zip_files(files_to_zip, zip_file_name, archive_file_names)
 
 #Zip files for the processLatestActivity cloud function
-files_to_zip = ['activityFunctions.py', 'config.json', 'config.py', 'credentials.json', 'googleSheetsAPI.py', 'main_processLatestActivity.py', 'requirements.txt', 'StravaAPI.py', 'token.json']
-archive_file_names = ['activityFunctions.py', 'config.json', 'config.py', 'credentials.json', 'googleSheetsAPI.py', 'main.py', 'requirements.txt', 'StravaAPI.py', 'token.json']
+files_to_zip = ['activityFunctions.py', 'config.json', 'config.py', 'credentials.json', 'googleSheetsAPI.py', 'main_processLatestActivity.py', 'requirements.txt', 'StravaAPI.py', 'token.json', 'firebaseServiceAccountKey.json']
+archive_file_names = ['activityFunctions.py', 'config.json', 'config.py', 'credentials.json', 'googleSheetsAPI.py', 'main.py', 'requirements.txt', 'StravaAPI.py', 'token.json', 'firebaseServiceAccountKey.json']
 zip_file_name = 'processLatestActivity.zip'
 zip_files(files_to_zip, zip_file_name, archive_file_names)
--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 import json
 import firebase_admin
 from firebase_admin import credentials, firestore
-from os import path
+import os
 
-if path.exists('firebaseServiceAccountKey.json'):
+def is_running_on_gcp():
+    return os.getenv('FUNCTION_TARGET') is not None
+
+#Manage settings with Firestore only if running on GCP
+if is_running_on_gcp():
     cred = credentials.Certificate('firebaseServiceAccountKey.json')
     firebase_admin.initialize_app(cred)
     db = firestore.client()
@@ -23,7 +27,8 @@ class Config:
         self.google_functions_url = data.get("google_functions_url")
 
 def getConfig() -> Config:
-    if path.exists('firebaseServiceAccountKey.json'):
+    
+    if is_running_on_gcp():
         config_data = doc_ref.get().to_dict()
 
     else:
@@ -33,21 +38,28 @@ def getConfig() -> Config:
     config = Config(config_data)
     return config
 
+
 def get(parameter: str):
-    if path.exists('firebaseServiceAccountKey.json'):
+    
+    if is_running_on_gcp():
         data = doc_ref.get().to_dict()
+    
     else:
         with open('config.json') as file:
             data = json.load(file)
+    
     return data[parameter]
 
+
 def write(parameter: str, value):
-    if path.exists('firebaseServiceAccountKey.json'):
+    
+    if is_running_on_gcp():
         doc_ref.update({parameter: value})
 
-    with open('config.json', 'r+') as file:
-        data = json.load(file)
-        data[parameter] = value
-        file.seek(0)
-        json.dump(data, file, indent=4)
-        file.truncate()
+    else:
+        with open('config.json', 'r+') as file:
+            data = json.load(file)
+            data[parameter] = value
+            file.seek(0)
+            json.dump(data, file, indent=4)
+            file.truncate()
